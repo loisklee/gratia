a0b64c7db7fed6ce18c548f86d6258431cadebbc
Lois Lee <lois.lee@me.com>
2019-08-02 16:40:10 -0400

    get edit entry to work

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -46,8 +46,8 @@ class EntriesController < ApplicationController
 		require_login		
 		@entry = Entry.find_by_id(params[:id])
 		is_current_user 
-		@entry.update(thoughts: params["thoughts"].strip, gratitude1: params["gratitude1"], gratitude2: params["gratitude2"], gratitude3: params["gratitude3"], gratitude4: params["gratitude4"], gratitude5: params["gratitude5"], reflection1: params["reflection1"], reflection2: params["reflection2"], reflection3: params["reflection3"], reflection4: params["reflection4"], reflection5: params["reflection5"], hopes1: params["hopes1"], hopes2: params["hopes2"], hopes3: params["hopes3"], hopes4: params["hopes4"], hopes5: params["hopes5"], user_id: current_user.id)				
-		redirect "/entries/show/#{@entry.id}"
+		@entry.update(thoughts: params["thoughts"].strip, gratitude1: params["gratitude1"], gratitude2: params["gratitude2"], gratitude3: params["gratitude3"], gratitude4: params["gratitude4"], gratitude5: params["gratitude5"], gratitude6: params["gratitude6"], gratitude7: params["gratitude7"], gratitude8: params["gratitude8"], gratitude9: params["gratitude9"], gratitude10: params["gratitude10"], reflection1: params["reflection1"], reflection2: params["reflection2"], reflection3: params["reflection3"], reflection4: params["reflection4"], reflection5: params["reflection5"], hopes1: params["hopes1"], hopes2: params["hopes2"], hopes3: params["hopes3"], hopes4: params["hopes4"], hopes5: params["hopes5"], user_id: current_user.id)				
+		redirect "/entries/#{@entry.id}"
 	end
 
 	delete '/entries/:id/delete' do
