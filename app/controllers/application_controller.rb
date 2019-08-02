9bd733280b36c092479607608e63029901c6a6ef
Lois Lee <lois.lee@me.com>
2019-08-01 22:53:48 -0400

    enable sessions, add default route of main

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,28 @@
 require './config/environment'
+require 'sinatra/flash'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+    register Sinatra::Flash
   end
 
   get "/" do
-    erb :welcome
+    erb :main
   end
 
+  helpers do 
+		
+		def logged_in?
+			!!current_user
+		end 
+		
+		def current_user
+			@current_user ||= User.find_by(id: session[:user_id])
+		end
+  end
 end
