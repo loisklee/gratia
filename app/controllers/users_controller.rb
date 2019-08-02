d5d74abfc5e9b854ff9c660263e96929bb0066b3
Lois Lee <lois.lee@me.com>
2019-08-02 15:49:35 -0400

    get login and register views and controller working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,65 +1,61 @@
+require 'pry'
+
 class UsersController < ApplicationController
-
-  get '/about'do
-    erb :about
-  end
-
-  get '/welcome' do
-    @user = current_user
-    erb :welcome
-  end
-  
-  get '/logout' do
-    session.clear
-    redirect to '/'
-  end
-  
-  get '/login' do
-    redirect to 'welcome' if logged_in?
-    erb :'users/login'
-  end
-
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
-  end
-
-  get '/register' do
-    redirect to '/welcome' if logged_in?
-    erb :"/users/new.html"
-  end
-
-  post '/register' do
-    is_empty?(params, 'register')
-
-    if User.find_by(:email => params[:email])
-        flash[:message] = "(This email already exists. Please enter a new email or log in to continue.)"
-        redirect to '/register'
+    get '/about'do
+        erb :about
     end
 
-    new_user = User.create(params)
-    session[:user_id] = new_user.id
-    redirect to '/welcome'
-end
+    get '/register' do
+        redirect to '/welcome' if logged_in?
+        erb :'users/register'
+    end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
+    post '/register' do
+        is_empty?(params, 'register')
 
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
+        if User.find_by(:email => params[:email])
+            flash[:message] = "(This email already exists. Please enter a new email or log in to continue.)"
+            redirect to '/register'
+        end
 
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
+        new_user = User.create(params)
+        session[:user_id] = new_user.id
+        redirect to '/welcome'
+    end
 
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
-  
+    get '/login' do
+        redirect to 'welcome' if logged_in?
+        erb :'users/login'
+    end
+
+    post '/login' do
+        is_empty?(params, 'login')
+
+        user = User.find_by(:email => params[:email])
+
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect to '/welcome'
+        else
+            if user
+                flash[:message] = "Your password is incorrect; please try again."
+                redirect to '/login'
+            else
+                flash[:message] = "The email and password did not match our system records. Please try again or sign up."
+                redirect to '/login'
+            end
+        end
+    end
+
+    get '/welcome' do
+        require_login
+        @user = current_user
+        erb :welcome
+    end
+
+    get '/logout' do
+        session.clear
+        redirect to '/'
+    end
+
 end
